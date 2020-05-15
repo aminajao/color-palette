1a1ceab81969432c8547c64c68fb5e9cf378d48a
amin ajao <aminajao96@gmail.com>
2020-05-15 21:53:18 +0100

    included a spinner to refetch data

--- screens/Home.jsx
+++ screens/Home.jsx
@@ -1,14 +1,14 @@
 import React, { useState, useEffect, useCallback } from 'react';
 import { StyleSheet, FlatList } from 'react-native';
 import PalettePreview from '../components/PalettePreview';
-
-
+import { RefreshControl } from 'react-native';
 
 const URL = 'https://color-palette-api.kadikraman.now.sh/palettes';
 
-
 const Home = ({ navigation }) => {
   const [palettes, setPalettes] = useState([]);
+  const [isRefreshing, setIsRefreshing] = useState(false);
+
 
   const handleFetchPalettes = useCallback(async () => {
     const response = await fetch(URL);
@@ -18,12 +18,21 @@ const Home = ({ navigation }) => {
     }
   }, []);
 
+
+  const handleRefresh = useCallback(async () => {
+    setIsRefreshing(true);
+    await handleFetchPalettes();
+    setIsRefreshing(false);
+  });
+  
+
   useEffect(() => {
     handleFetchPalettes();
   }, []);
 
   return (
     <FlatList
+      refreshControl={<RefreshControl refreshing={isRefreshing} onRefresh={handleRefresh} />}
       data={palettes}
       keyExtractor={(item) => item.paletteName}
       renderItem={({ item }) => (
@@ -37,7 +46,5 @@ const Home = ({ navigation }) => {
     />
   );
 };
-
-
 
 export default Home;
